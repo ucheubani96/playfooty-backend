111a2d1ed14033d0fa037209b5a83500f95268a8
ucheubani96 <mac@Joey-Macbook.local>
2025-04-14 15:12:19 +0100

    Setup Logging and monitoring

--- backend-api/src/main/java/com/playfooty/backend_api/repositories/UserDetailsRepo.java
+++ backend-api/src/main/java/com/playfooty/backend_api/repositories/UserDetailsRepo.java
@@ -2,8 +2,10 @@ package com.playfooty.backend_api.repositories;
 
 import com.playfooty.backend_api.models.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface UserDetailsRepo extends JpaRepository<UUID, UserDetails> {
+@Repository
+public interface UserDetailsRepo extends JpaRepository<UserDetails, UUID> {
 }
